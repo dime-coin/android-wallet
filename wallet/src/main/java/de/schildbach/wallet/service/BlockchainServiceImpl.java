71790ba7b99ad915d66d570b9e46391175f0b397
moondoggo <anthony@sureazure.com>
2018-04-16 15:03:57 -0700

    remove device vibration on peer connection notification

--- wallet/src/main/java/de/schildbach/wallet/service/BlockchainServiceImpl.java
+++ wallet/src/main/java/de/schildbach/wallet/service/BlockchainServiceImpl.java
@@ -295,6 +295,7 @@ public class BlockchainServiceImpl extends android.app.Service implements Blockc
 					{
 						final NotificationCompat.Builder notification = new NotificationCompat.Builder(BlockchainServiceImpl.this, DIMECOIN_NOTIFICATIONS);
 						notification.setSmallIcon(R.drawable.stat_sys_peers, numPeers > 4 ? 4 : numPeers);
+						notification.setVibrate(new long[]{0L});
 						notification.setContentTitle(getString(R.string.app_name));
 						notification.setContentText(getString(R.string.notification_peers_connected_msg, numPeers));
 						notification.setContentIntent(PendingIntent.getActivity(BlockchainServiceImpl.this, 0, new Intent(BlockchainServiceImpl.this,
